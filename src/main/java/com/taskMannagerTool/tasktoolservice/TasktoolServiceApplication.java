b4bb8e8745b13fe8d7e14905926d651b8dba772c
snowtoslow <snowtoslow@mail.ru>
2019-10-23 13:23:37 +0300

    Initial commit

---
+++ src/main/java/com/taskMannagerTool/tasktoolservice/TasktoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taskMannagerTool.tasktoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktoolServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskMannagerTool/tasktoolservice/TasktoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskMannagerTool.tasktoolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktoolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
