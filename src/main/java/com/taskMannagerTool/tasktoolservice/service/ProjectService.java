35eecc923103f5219b8779861852847dd0edadb2
SummonCow <43407249+SummonCow@users.noreply.github.com>
2019-10-31 12:16:59 +0300

    Add project controller;                                                                                                    Rename to ProjectServiceImpl;                                                                                                    Create interface Project service;

--- src/main/java/com/taskMannagerTool/tasktoolservice/service/ProjectService.java
+++ src/main/java/com/taskMannagerTool/tasktoolservice/service/ProjectService.java
@@ -1,62 +1,19 @@
 package com.taskMannagerTool.tasktoolservice.service;
 
 import com.taskMannagerTool.tasktoolservice.models.Project;
-import com.taskMannagerTool.tasktoolservice.repository.ProjectRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
-@RestController
-public class ProjectService {
+public interface ProjectService {
 
-    @Autowired
-    private ProjectRepository projectRepository;
+    ResponseEntity<Object> createProject(Project project);//C
 
-    @PostMapping("/projects")
-    public ResponseEntity<Object> createProject(@RequestBody Project project){//добавить респонс бади
-        Project savedProject = projectRepository.save(project);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{taskId}")
-                .build(savedProject.getProjectId());
+    List<Project> readAllProjects();//R
 
-        return ResponseEntity.created(location).build();
-    }
+    Project readAProjectById(int ProjectId);//R
 
-    @GetMapping("/projects")//Read
-    public List<Project> readAllProjects(){
-        return projectRepository.findAll();
-    }
+    ResponseEntity<Object> updateProject(Project project, int ProjectId);//U !!!!!пересмотреть в имплементэйшне!!!
 
-    //Read a specific task by Id;
-    @GetMapping("/projects/{projectId}")
-    public Project readAProjectById(@PathVariable int projectId){
-        Optional<Project> project = projectRepository.findById(projectId);//костыль надо поменять
-
-        if (!project.isPresent()){
-            System.out.println("There is no project with such id: "+projectId);
-        }
-        return project.get();
-    }
-
-
-    @PutMapping("/projects/{projectId}")
-    public ResponseEntity<Object> updateProject(@RequestBody Project project, @PathVariable int projectId){
-        Optional<Project>  projectOptional = projectRepository.findById(projectId);
-
-        if(!projectOptional.isPresent())
-            return ResponseEntity.notFound().build();
-        project.setProjectId(projectId);
-        projectRepository.save(project);
-        return ResponseEntity.noContent().build();
-    }
-
-    @DeleteMapping("projects/{projectId}")
-    public void deleteTask(@PathVariable int projectId){
-        projectRepository.deleteById(projectId);
-    }
-
+    void deleteProject(int projectId);//D
 }
---
+++ src/main/java/com/taskMannagerTool/tasktoolservice/service/ProjectServiceImpl.java
@@ -0,0 +1,62 @@
+package com.taskMannagerTool.tasktoolservice.service;
+
+import com.taskMannagerTool.tasktoolservice.models.Project;
+import com.taskMannagerTool.tasktoolservice.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjectServiceImpl {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+
+    public ResponseEntity<Object> createProject( Project project){//добавить респонс бади
+        Project savedProject = projectRepository.save(project);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{taskId}")
+                .build(savedProject.getProjectId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+        public List<Project> readAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    //Read a specific task by Id;
+
+    public Project readAProjectById( int projectId){
+        Optional<Project> project = projectRepository.findById(projectId);//костыль надо поменять
+
+        if (!project.isPresent()){
+            System.out.println("There is no project with such id: "+projectId);
+        }
+        return project.get();
+    }
+
+
+
+    public ResponseEntity<Object> updateProject( Project project, int projectId){
+        Optional<Project>  projectOptional = projectRepository.findById(projectId);
+
+        if(!projectOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        project.setProjectId(projectId);
+        projectRepository.save(project);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    public void deleteProject( int projectId){
+        projectRepository.deleteById(projectId);
+    }
+
+}
