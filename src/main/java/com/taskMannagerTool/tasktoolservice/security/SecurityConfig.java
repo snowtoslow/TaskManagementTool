733f5407fd7b157e6f3670379685915e9c4ecb2f
snowtoslow <snowtoslow@mail.ru>
2019-11-06 22:59:21 +0200

    made changes for new database;change DB name;

--- src/main/java/com/taskMannagerTool/tasktoolservice/security/SecurityConfig.java
+++ src/main/java/com/taskMannagerTool/tasktoolservice/security/SecurityConfig.java
@@ -1,40 +1,19 @@
 package com.taskMannagerTool.tasktoolservice.security;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("universal").password(passwordEncoder().encode("qwerty123")).roles("ADMIN");
-        }
-
-        // Secure the endpoins with HTTP Basic authentication
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http
-                    .authorizeRequests()
-                    .anyRequest().permitAll()
-                    .and()
-                    .httpBasic();
-            http.csrf().disable();
-        }
-        @Bean
-        PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
 }
 
